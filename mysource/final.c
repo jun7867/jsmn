68f32cff3e0518ae2949e15b38877b0296bd126b
jun7867 <jjune095@naver.com>
2018-06-09 02:30:23 +0900

    final.c example8 practice

--- mysource/final.c
+++ mysource/final.c
@@ -57,19 +57,55 @@ void printNameList(char *JSON_STRING,jsmntok_t *t,int *nameTokIndex){
 	}
 }
 
-void selecNameList(char *JSON_STRING,jsmntok_t *t,int *nameTokIndex){
+// void selecNameList(char *JSON_STRING,jsmntok_t *t,int *nameTokIndex){
+// 	int i=0;
+// 	int select;
+// 	int s=0;
+// 	while(1){
+// 	printf("Select Name's no : (exit : 0) \n");
+// 	scanf("%d",&select);
+// 	if(select==0) break;
+// 	s=nameTokIndex[select-1];
+// 	printf("[NAME %d] %.*s \n",select,t[s].end-t[s].start,JSON_STRING + t[s].start);
+// 	printf(" %.*s \n",t[s+1].end-t[s+1].start,JSON_STRING + t[s+1].start);
+//
+// }
+// }
+void objectNameList(char *JSON_STRING,jsmntok_t *t,int tokcount,int *objectIndex)
+{
 	int i=0;
-	int select;
-	int s=0;
-	while(1){
-	printf("Select Name's no : (exit : 0) \n");
-	scanf("%d",&select);
-	if(select==0) break;
-	s=nameTokIndex[select-1];
-	printf("[NAME %d] %.*s \n",select,t[s].end-t[s].start,JSON_STRING + t[s].start);
-	printf(" %.*s \n",t[s+1].end-t[s+1].start,JSON_STRING + t[s+1].start);
+	int j=0;
+	printf("********OBJECT LIST******\n");
+	for(i=0;i<tokcount;i++){
+		if(t[0].type==JSMN_ARRAY){ //type이 array로 시작하면 아래식 진행.
+			if(t[i].type==JSMN_OBJECT && t[i-1].size==0 || t[i-1].type==2 ){
+				printf("[NAME %d] %.*s \n",j+1,t[i+2].end-t[i+2].start,JSON_STRING + t[i+2].start); // i+2인 이유는 object에서 +2인것이 value이기 떄문에.
+				j++; // 출력된것의 번호를 알기 위한것.
+			}
+		}
+		else{ //시작이 array가 아닐때
+			if(t[i].type==JSMN_OBJECT && t[i-1].size==0 && t[t[i+1].parent].parent<0){ // 토큰의 타입이 오브젝트일때 4개가 나오는데 거기서 그 전의 사이즈가 0인것이 큰 오브젝트다.
 
-}
+				if(t[i+2].type==2){ // i가 {로 시작하는값이므로 그것의 +2 즉 첫번째 name의 다음이 어레이면 실행.(data4.json일때)
+					for(i+=1;i<tokcount;i++){
+					if(t[i].type==JSMN_OBJECT && t[i-1].size==0 || t[i-1].type==2){
+						printf("[NAME %d] %.*s \n",j+1,t[i+2].end-t[i+2].start,JSON_STRING + t[i+2].start); // i+2인 이유는 object에서 +2인것이 value이기 떄문에.
+						j++; // 출력된것의 번호를 알기 위한것.
+						}
+					}
+				}
+				else{
+					if(t[i].type==JSMN_OBJECT && t[i-1].size==0 && t[t[i+1].parent].parent<0){
+						printf("[NAME %d] %.*s \n",j+1,t[i+2].end-t[i+2].start,JSON_STRING + t[i+2].start); // i+2인 이유는 object에서 +2인것이 value이기 떄문에.
+						j++; // 출력된것의 번호를 알기 위한것.
+				}
+			}
+
+				// printf("[NAME %d] %.*s \n",j+1,t[i+2].end-t[i+2].start,JSON_STRING + t[i+2].start); // i+2인 이유는 object에서 +2인것이 value이기 떄문에.
+				// j++; // 출력된것의 번호를 알기 위한것.
+			}
+		}
+	}
 }
 
 int main() {
@@ -92,10 +128,12 @@ int main() {
 		return 1;
 	}
 	int nameIndex[100]={0};
+	int objectIndex[100]={0};
 
-	jsonNamelist(JSON_STRING,t,r,nameIndex);
-	printNameList(JSON_STRING,t,nameIndex);
-	selecNameList(JSON_STRING,t,nameIndex);
+	// jsonNamelist(JSON_STRING,t,r,nameIndex);
+	// printNameList(JSON_STRING,t,nameIndex);
+	// selecNameList(JSON_STRING,t,nameIndex);
+	objectNameList(JSON_STRING,t,r,objectIndex);
 	/* Loop over all keys of the root object */
 	// for (i = 1; i < r; i++) {
 	// 	if (jsoneq(JSON_STRING, &t[i], "name") == 0) {
